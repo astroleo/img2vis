with correct PA rotation and nice plotting of results

img2vis.py
@@ -9,6 +9,7 @@ from azimuthalAverage import azimuthalAverage
 from astropy.io import fits
 from scipy.interpolate import RectBivariateSpline
 from scipy.ndimage.interpolation import rotate
+from scipy.ndimage import map_coordinates
 
 import pdb
 
@@ -28,10 +29,10 @@ def read_midi_oifits(f,lam,dlam,phot=False):
 	v=hdu[4].data
 
 	if phot:
-		raise ValueError("Need to check computation of errors in photometric mode...")
-	
 		vv=v["CFLUX"]
 		vv/=phot
+		vv_noise=v["CFLUXERR"]
+		vv_noise/=phot
 	else:
 		vv = v["VISAMP"]
 		vv_noise = v["VISAMPERR"]
@@ -54,8 +55,13 @@ def read_midi_oifits(f,lam,dlam,phot=False):
 ## NOTE: we are not using the class variables here but the possibly modified local variables
 ##       vis, fftscale (modified by vis_chi2)
 ##
+## NOTE about coordinate systems: we are only plotting and using the "right", 
+##    i.e. negative "u" side of the (u,v) plane, but pixel coordinates are positive
+##    from the origin, i.e. we need to invert the u coordinate here.
+##
 def modelvis(u,v,vis,fftscale,roll):
 	## round to nearest pixel position in image
+	u=-u
 	x=np.round(u/fftscale)
 	y=roll+np.round(v/fftscale)
 	if u<0:
@@ -66,28 +72,28 @@ def modelvis(u,v,vis,fftscale,roll):
 
 class img2vis():
 	"""
-	A class to convert model surface brightness distributions to visibilities, make nice plots and adjust PA and scale so that the model matches with observed data
+	A class to convert model surface brightness distributions to visibilities, 
+	make nice plots and adjust PA and scale so that the model matches with observed data
 	
 	NOTES
-		PA is defined east of west, i.e. counter-clockwise on sky
-			here we treat the image as an image on sky, i.e. pixel coordinates = - RA coordinates
-			(the RA axis increases to the left, the pixel coordinates increase to the right)
-			Since the same is true for both image and (u,v) plane, we just relabel to RA / (u,v) coordinates
-			at the end and keep the image and the fourier transform in pixel space.
-
+		PA is defined east of north, i.e. counter-clockwise on sky
+		here we treat the image as an image on sky, i.e. pixel coordinates = - RA coordinates
+		(the RA axis increases to the left, the pixel coordinates increase to the right)
+		Since the same is true for both image and (u,v) plane, we just relabel to RA / (u,v) coordinates
+		at the end and keep the image and the fourier transform in pixel space.
 	
 	PARAMETERS
-	   f_model   path to FITS file of input model image
-	   pxscale   pixel scale in milli-arcseconds (mas)
-	   lam       wavelength in meters
+		f_model		path to FITS file of input model image
+		pxscale		pixel scale in milli-arcseconds (mas)
+		lam			wavelength in meters
 	
 	OPTIONAL PARAMETERS
-	   oifits    path to OIFITS file containing visibilities for this object
-	   phot      if OIFITS file has a CFLUX field (assuming: [Jy]), compute visibility with this total flux [Jy]
-	   nikutta   Robert's models are stored in a particular FITS extension and need to be padded, 
-						set to True when 	visualizing his models
-		pa		Position Angle (east of west / counter-clockwise on sky) that the input image will be rotated before transforming it
-		binary	if set to True, will add a binary to the image to test if the transform / plotting etc. works
+		oifits		path to OIFITS file containing visibilities for this object
+		phot		if OIFITS file has a CFLUX field (assuming: [Jy]), compute visibility with this total flux [Jy]
+		nikutta		Robert's models are stored in a particular FITS extension and need to be padded, 
+						set to True when visualizing his models
+		pa			Position Angle (east of north / counter-clockwise on sky) that the input image will be rotated before transforming it
+		binary		if set to True, will add a binary to the image to test if the transform / plotting etc. works
 	"""
 	
 	
@@ -97,6 +103,7 @@ class img2vis():
 		self.lam=lam
 		self.oifits=oifits
 		self.nikutta=nikutta
+		self.pa_best=False
 		##
 		## print some info
 		print("Pixel scale: ", pxscale, " mas per pixel px")
@@ -127,12 +134,25 @@ class img2vis():
 		if binary:
 			##
 			## add point source to image (for testing purposes)
-			binsep_mas = 10
-			binsep_px = binsep_mas / pxscale
+			binsep_mas=10
+			binsep_px=binsep_mas / pxscale
 			print("Binary separation: ", binsep_mas, " mas")
-			self.img[250,250] += 100000
-			self.img[250,250+binsep_px] += 100000
+			self.img[250,250]+=100000
+			self.img[250,250+binsep_px]+=100000
 
+		##
+		## perform the actual FFT
+		self.rotate_and_fft(pa,verbose=True)
+		
+		##
+		## read observed data
+		if self.oifits:
+			if phot:
+				self.bl,self.pa,self.u,self.v,self.vis_obs,self.vis_obs_noise=read_midi_oifits(oifits,lam,dlam,phot=phot)
+			else:
+				self.bl,self.pa,self.u,self.v,self.vis_obs,self.vis_obs_noise=read_midi_oifits(oifits,lam,dlam,phot=False)
+
+	def rotate_and_fft(self,pa,verbose=False):
 		## "rotate" rotates clockwise (careful: matplotlib.imshow uses origin="upper" per default)
 		##  PA is counter-clockwise on sky (see note above for coordinate systems)
 		self.img=rotate(self.img, -pa, reshape=True)
@@ -142,8 +162,8 @@ class img2vis():
 		##
 		## =============== compute FFT frequencies and scale ===============
 		##
-		fft_freq = np.fft.fftfreq(gridsize,pxscale)
-		fft_img = np.abs(np.fft.rfft2(self.img))
+		fft_freq=np.fft.fftfreq(gridsize,self.pxscale)
+		fft_img=np.abs(np.fft.rfft2(self.img))
 		##
 		## determine norm for visibilities, roll axes so that values start at 0 freq.
 		self.roll=np.round(fft_img.shape[0]/2)
@@ -153,17 +173,12 @@ class img2vis():
 		freq=np.roll(fft_freq,r,0)
 		##
 		## pxscale -> fftscale
-		fftscale = np.diff(freq)[0] ## cycles / mas per pixel in FFT image
+		fftscale=np.diff(freq)[0] ## cycles / mas per pixel in FFT image
 		mas2rad=np.deg2rad(1/3600000) ## mas per rad
-		self.fftscale = fftscale/mas2rad * lam ## meters baseline at given lam per px in FFT image
-		print("Pixel scale in FFT image is: ", fftscale, " m (Baseline) per pixel")
-		##
-		## read observed data
-		if oifits:
-			if phot:
-				self.bl,self.pa,self.u,self.v,self.vis_obs,self.vis_obs_noise=read_midi_oifits(oifits,lam,dlam,phot=phot)
-			else:
-				self.bl,self.pa,self.u,self.v,self.vis_obs,self.vis_obs_noise=read_midi_oifits(oifits,lam,dlam,phot=False)
+		self.fftscale = fftscale/mas2rad * self.lam ## meters baseline per px in FFT image at given wavelength
+		if verbose:
+			print("Pixel scale in FFT image is: ", self.fftscale, " m (Baseline) per pixel")
+
 
 	##
 	## METHOD vis_chi2
@@ -186,7 +201,22 @@ class img2vis():
 			chi2 += (vis_obs - modelvis(u,v,self.vis,self.fftscale,self.roll))**2/vis_obs_noise**2
 		return chi2
 
+	def optimize_pa(self):
+		self.pas=[]
+		self.chi2=[]
 
+		for pa in np.arange(18)*10:
+			##
+			## this could be made more efficient by only calling the sub-routine rotate_and_fft
+			## but then need to watch out for differential vs. absolute rotations
+			i=img2vis(self.f_model, self.pxscale, self.lam, oifits=self.oifits, pa=pa)
+			self.chi2.append(i.vis_chi2())
+			self.pas.append(pa)
+		
+		## chose global chi2 minimum here for the moment, and plot PA vs. chi2 
+		##    so that we see if global minimum is bad.
+		self.pa_best = self.pas[np.argmin(self.chi2)]
+		self.rotate_and_fft(self.pa_best)
 
 	##
 	## METHOD make_plot
@@ -195,7 +225,7 @@ class img2vis():
 	##
 	def make_plot(self):
 		##
-		## =============== make nice plot ===============
+		## =============== model image ===============
 		##
 		plt.subplot(221)
 		## cut out central region
@@ -223,7 +253,9 @@ class img2vis():
 		plt.xlabel("x [mas]")
 		plt.ylabel("y [mas]")
 
-
+		##
+		## =============== model visibilities on (u,v) plane ===============
+		##
 		plt.subplot(222)
 		plt.imshow(self.vis,origin="lower")
 		plt.colorbar(label="Visibility amplitude")
@@ -249,46 +281,128 @@ class img2vis():
 		xylim=np.round(max_bl/self.fftscale)
 		plt.xlim([0,xylim])
 		plt.ylim([self.roll-xylim,self.roll+xylim])
+		
+		##
+		## choose two axes to show radial profiles, one along pa_best and one perpendicular to that
+		## following http://stackoverflow.com/questions/7878398/how-to-extract-an-arbitrary-line-of-values-from-a-numpy-array
+		r_m = 100 ## length of line in meters
+		r_px = r_m/self.fftscale ## length of line in px coordinates
+		num = r_m ## number of points to choose for interpolation
+		if not self.pa_best:
+			self.pa_best=0
+
+		x0, y0 = 0, self.roll
+		x1, y1 = np.sin(np.pi/180 * (90 - self.pa_best)) * r_px, np.cos(np.pi/180 * (90 - self.pa_best)) * r_px + self.roll
+		x,y = np.linspace(x0,x1,num), np.linspace(y0,y1,num)
+		visi1 = map_coordinates(self.vis, np.vstack((y,x))) ## interpolated model visibilities
+		plt.plot([x0,x1],[y0,y1],'ro-')
+		
+		x1, y1 = np.sin(np.pi/180 * (180 - self.pa_best)) * r_px, np.cos(np.pi/180 * (180 - self.pa_best)) * r_px + self.roll
+		x,y = np.linspace(x0,x1,num), np.linspace(y0,y1,num)
+		visi2 = map_coordinates(self.vis, np.vstack((y,x))) ## interpolated model visibilities
+		plt.plot([x0,x1],[y0,y1],'go-')
+		
+#		pdb.set_trace()
 
 
+		##
+		## =============== chi^2 values as a function of PA ===============
+		##
 		plt.subplot(223)
-		if self.oifits:
-			plt.plot(self.bl,self.vis_obs,'ks',label="MIDI data")
-			plt.legend(numpoints=1)
-		if self.nikutta:
-			r,V = azimuthalAverage(self.vis,center=[0,self.roll],returnradii=True,binsize=2)
+		if self.oifits:	
+			plt.plot(self.pas,self.chi2)
+			plt.plot([self.pa_best,self.pa_best],plt.ylim(),'r')
+			plt.xlabel("Position Angle (East of North)")
+			plt.ylabel("chi square")
+			plt.title("Optimal rotation of model")
 		else:
-			r,V = azimuthalAverage(self.vis,center=[0,self.roll],returnradii=True)
-	#	pdb.set_trace()
-		plt.plot(self.fftscale * r,V, label="model")
-		plt.ylim([0,1])
-		plt.xlim([0,130])
-		plt.xlabel("Projected baseline [m]")
-		plt.ylabel("Visibility amplitude")
-		plt.title("Azimuthally averaged visamp")
+			plt.plot(0,0)
+			plt.title("No data available")
 
+		##
+		## =============== radial cuts along specific PA ===============
+		##
+		plt.subplot(224)
+		
+		
+		
+		
+#		pdb.set_trace()
+		
+		
+		
+		
+		plt.plot(np.arange(num), visi1, 'r',label="Model at PA = {0}".format(90+self.pa_best))
+		plt.plot(np.arange(num), visi2, 'g',label="Model at PA = {0}".format(self.pa_best))
+		plt.xlabel("Projected Baseline length [m]")
+		plt.ylabel("Visibility")
+		
 		if self.oifits:
-			plt.subplot(224)
-		#	plt.plot(fftscale * np.arange(vis.shape[1]),vis[roll,:])
-		#	plt.ylim([0,1])
-		#	plt.xlim([0,130])
-		#	plt.xlabel("Projected baseline [m]")
-		#	plt.ylabel("Visibility amplitude")
-		#	plt.title("Meridional cut through (u,v) plane")
-			cNorm=c.Normalize(vmin=0,vmax=1.0)
-			scalarMap=cmx.ScalarMappable(norm=cNorm,cmap="rainbow")
-			for iu,iv,ivis in zip(self.u,self.v,self.vis_obs):
-				plt.plot(-iu,iv,color=scalarMap.to_rgba(ivis),mew=0,ms=4,marker="o")
-				plt.plot(iu,-iv,color=scalarMap.to_rgba(ivis),mew=0,ms=4,marker="o")
-			plt.xlim([-130,130])
-			plt.ylim([-130,130])
-			xyticks=np.array([-100,-50,0,50,100])
-			plt.xticks(xyticks,-xyticks)
-			plt.yticks(xyticks,xyticks)
-			plt.xlabel("u [m]")
-			plt.ylabel("v [m]")
-			plt.colorbar(label="Visibility amplitude")
-			plt.title("Observed (u,v) plane")
+			##
+			## overplot data with similar PA range
+			pa_pos = self.pa ## create array with only positive PAs
+			pa_pos[pa_pos < 0] += 180
+			if self.pa_best < 0:
+				pa_best += 180
+			else:
+				pa_best = self.pa_best
+
+#			if pa_best+90 > 180:
+#				raise ValueError("Need to think again...")
+
+		
+			delta_pa = 15 ## half-range of PA to use for PA selection of observed 
+			##               visibilities (i.e. data with PA +/- delta_pa will be chosen)
+		
+			ix = (pa_pos > (pa_best+90 - delta_pa)) & (pa_pos < (pa_best+90 + delta_pa))
+			plt.plot(self.bl[ix],self.vis_obs[ix],'rx',label="MIDI, PA = {0} +/- {1}".format(self.pa_best+90,delta_pa))
+
+			ix = (pa_pos > (pa_best - delta_pa)) & (pa_pos < (pa_best + delta_pa))
+			plt.plot(self.bl[ix],self.vis_obs[ix],'gx',label="MIDI, PA = {0} +/- {1}".format(self.pa_best,delta_pa))
+
+		plt.legend(numpoints=1,fontsize=8)
+		
+		
+		
+### below: old sub-plots showing azimuthally averaged visibilities (plot 3) and observed visibilities on (u,v) plane (plot 4)
+# 		plt.subplot(223)
+# 		if self.oifits:
+# 			plt.plot(self.bl,self.vis_obs,'ks',label="MIDI data")
+# 			plt.legend(numpoints=1)
+# 		if self.nikutta:
+# 			r,V = azimuthalAverage(self.vis,center=[0,self.roll],returnradii=True,binsize=2)
+# 		else:
+# 			r,V = azimuthalAverage(self.vis,center=[0,self.roll],returnradii=True)
+# 	#	pdb.set_trace()
+# 		plt.plot(self.fftscale * r,V, label="model")
+# 		plt.ylim([0,1])
+# 		plt.xlim([0,130])
+# 		plt.xlabel("Projected baseline [m]")
+# 		plt.ylabel("Visibility amplitude")
+# 		plt.title("Azimuthally averaged visamp")
+# 
+# 		if self.oifits:
+# 			plt.subplot(224)
+# 		#	plt.plot(fftscale * np.arange(vis.shape[1]),vis[roll,:])
+# 		#	plt.ylim([0,1])
+# 		#	plt.xlim([0,130])
+# 		#	plt.xlabel("Projected baseline [m]")
+# 		#	plt.ylabel("Visibility amplitude")
+# 		#	plt.title("Meridional cut through (u,v) plane")
+# 			cNorm=c.Normalize(vmin=0,vmax=1.0)
+# 			scalarMap=cmx.ScalarMappable(norm=cNorm,cmap="rainbow")
+# 			for iu,iv,ivis in zip(self.u,self.v,self.vis_obs):
+# 				plt.plot(-iu,iv,color=scalarMap.to_rgba(ivis),mew=0,ms=4,marker="o")
+# 				plt.plot(iu,-iv,color=scalarMap.to_rgba(ivis),mew=0,ms=4,marker="o")
+# 			plt.xlim([-130,130])
+# 			plt.ylim([-130,130])
+# 			xyticks=np.array([-100,-50,0,50,100])
+# 			plt.xticks(xyticks,-xyticks)
+# 			plt.yticks(xyticks,xyticks)
+# 			plt.xlabel("u [m]")
+# 			plt.ylabel("v [m]")
+# 			plt.colorbar(label="Visibility amplitude")
+# 			plt.title("Observed (u,v) plane")
 
 		plt.tight_layout()
 		plt.suptitle(self.f_model.split(".fits")[0] + str(" (lam={0} m)".format(self.lam)),fontsize=6)

model_definition.py
@@ -20,9 +20,9 @@ pas=[]
 chi2=[]
 
 for pa in np.arange(18)*10:
-	i=img2vis("../models/Bernd_2016-05-13/circinus_bild_10_25L0.111111.fits", pxscale_circ, 1e-5, oifits=oifitscirc, pa=-pa)
+	i=img2vis("../models/Bernd_2016-05-13/circinus_bild_10_25L0.111111.fits", pxscale_circ, 1e-5, oifits=oifitscirc, pa=pa)
 	chi2.append(i.vis_chi2())
-	pas.append(-pa)
+	pas.append(pa)
 
 pa_min_chi2=40
 i=img2vis("../models/Bernd_2016-05-13/circinus_bild_10_25L0.111111.fits", pxscale_circ, 1e-5, oifits=oifitscirc, pa=pa_min_chi2)